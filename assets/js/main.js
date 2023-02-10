6b393663d7c59f8604334e451d055156af4d56cf
Anand Sharma <ak2865483@gmail.com>
2023-02-10 15:23:57 +0530

    sadasdsa

--- assets/js/main.js
+++ assets/js/main.js
@@ -232,3 +232,69 @@ function closePopup() {
 window.onload = function() {
   setTimeout(showPopup, 5000); // show the popup after 5 seconds
 };
+
+
+// Video themailnal
+$(".video-play").on('click', function(e) {
+  e.preventDefault(); 
+  var vidWrap = $(this).parent(),
+      iframe = vidWrap.find('.video iframe'),
+      iframeSrc = iframe.attr('src'),
+      iframePlay = iframeSrc += "?autoplay=1";
+  vidWrap.children('.video-thumbnail').fadeOut();
+  vidWrap.children('.video-play').fadeOut();
+  vidWrap.find('.video iframe').attr('src', iframePlay);
+
+
+});
+
+
+
+// Gallary
+$(document).ready(function()
+{
+
+   
+        if($('.bbb_viewed_slider').length)
+        {
+            var viewedSlider = $('.bbb_viewed_slider');
+
+            viewedSlider.owlCarousel(
+            {
+                loop:true,
+                margin:30,
+                autoplay:true,
+                autoplayTimeout:6000,
+                nav:false,
+                dots:false,
+                responsive:
+                {
+                    0:{items:1},
+                    575:{items:2},
+                    768:{items:3},
+                    991:{items:4},
+                    1199:{items:6}
+                }
+            });
+
+            if($('.bbb_viewed_prev').length)
+            {
+                var prev = $('.bbb_viewed_prev');
+                prev.on('click', function()
+                {
+                    viewedSlider.trigger('prev.owl.carousel');
+                });
+            }
+
+            if($('.bbb_viewed_next').length)
+            {
+                var next = $('.bbb_viewed_next');
+                next.on('click', function()
+                {
+                    viewedSlider.trigger('next.owl.carousel');
+                });
+            }
+        }
+
+
+    });
